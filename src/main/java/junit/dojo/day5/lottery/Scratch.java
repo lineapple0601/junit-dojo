c157421e8a6a6e7a1297a08232b8f0ce7505c4e0
申昇祐 <lineapple0601@gmail.com>
2022-02-17 16:24:45 +0900

    JavaPlayGroundの内容

---
+++ src/main/java/junit/dojo/day5/lottery/Scratch.java
@@ -0,0 +1,45 @@
+package junit.dojo.day5.lottery;
+
+import java.util.Random;
+import junit.dojo.day5.IScratch;
+
+/**
+ * スクラッチくじを実装したクラスです。
+ * {@code LotteryFactory}以外では
+ * インスタンスを作成できないようpackage privateクラスにしています。
+ *
+ * @see LotteryFactory
+ * @author autotaker
+ *
+ */
+class Scratch implements IScratch {
+	int[][] sheet;
+
+	Scratch(Random rng) {
+		sheet = new int[3][3];
+		for (int row = 0; row < 3; row++) {
+			for(int col = 0; col < 3; col++) {
+				sheet[row][col] = rng.nextInt(9) + 1;
+			}
+		}
+	}
+
+	@Override
+	public int getValue(int row, int col) {
+		return sheet[row][col];
+	}
+
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (int[] rowItem : sheet) {
+			for( int digit: rowItem) {
+				sb.append(digit);
+			}
+			sb.append('\n');
+		}
+		return sb.toString();
+	}
+
+}
