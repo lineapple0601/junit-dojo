49cbd6c6fbbd335df092d1afccad6c7b4524aaa7
申昇祐 <lineapple0601@gmail.com>
2022-02-25 14:17:27 +0900

    no message

--- src/main/java/junit/dojo/day6/HogeProcessor.java
+++ src/main/java/junit/dojo/day6/HogeProcessor.java
@@ -3,9 +3,12 @@ package junit.dojo.day6;
 public class HogeProcessor {
 	
         public String process(String fileType) {
-                String value = HogeSingleton.instance().getValue(fileType);
+                String value = createHogeSingletonValue(fileType);
                 // いろいろやる的な
                 return value.toLowerCase() + "_mod";
         }
         
+        protected String createHogeSingletonValue(String fileType) {
+        	return HogeSingleton.instance().getValue(fileType);
+        }
 }
